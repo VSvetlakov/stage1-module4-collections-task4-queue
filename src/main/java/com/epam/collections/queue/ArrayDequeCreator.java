54acc414804aacbfd77fc4a81f8b18d020bff710
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-12-09 10:09:06 +0400

    v.1

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,36 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        takeTwo(deque, firstQueue);
+
+        takeTwo(deque, secondQueue);
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()){
+            if (!firstQueue.isEmpty())
+                nextTurn(deque, firstQueue);
+
+            if (!secondQueue.isEmpty())
+                nextTurn(deque, secondQueue);
+        }
+
+        return deque;
+    }
+
+    public void nextTurn(ArrayDeque<Integer> deque, Queue<Integer> queue){
+        queue.add(deque.pollLast());
+        for (int i = 0; i < 2; i++) {
+            if(!queue.isEmpty()) {
+                deque.add(queue.poll());
+            }
+        }
+    }
+
+    public void takeTwo(ArrayDeque<Integer> deque, Queue<Integer> queue){
+        for (int i = 0; i < 2; i++) {
+            deque.add(queue.poll());
+        }
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,39 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        List<Integer> list = new ArrayList<>();
+
+        Queue<Integer> queue = initDisesQueu(numberOfDishes);
+
+        int curNumberDish = 1;
+
+        while (!queue.isEmpty()) {
+
+            int curDish = queue.poll();
+
+            if (curNumberDish % everyDishNumberToEat == 0) {
+                list.add(curDish);
+            } else {
+                queue.add(curDish);
+            }
+
+            curNumberDish++;
+        }
+
+        return list;
+    }
+
+    private Queue<Integer> initDisesQueu(int numberOfDishes){
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        return queue;
     }
 }
